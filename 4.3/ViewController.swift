ee87e4e614022c233fffe82420624bc219043add
student <student@iMac-64.local>
2018-12-15 14:28:48 +0800

    Initial commit

--- 4.3/ViewController.swift
+++ 4.3/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //声明UILabel对象
+    var label: UILabel!
+    var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+      
+        //创建UILabel对象
+        label = UILabel(frame: CGRect(x: 50, y: 200, width: 300, height: 40))
+        //设置label显示文字
+        label.text = "hello world"
+        label.font = UIFont.systemFont(ofSize: 22)
+        label.textColor = UIColor.black
+        //将label加入视图控制器
+        self.view.addSubview(label)
+        
+        //创建UIButton对象
+        let button = UIButton(frame: CGRect(x: 50, y: 50, width: 100, height: 50))
+        //设置正常状态下按钮的标题
+        button.setTitle("click me", for: .normal)
+        //设置按钮边框宽度
+        button.layer.borderWidth = 1
+        //设置正常状态下标题的颜色
+        button.setTitleColor(UIColor.black, for: .normal)
+        //设置高亮状态下（被点击且未释放鼠标）标题的颜色
+        button.setTitleColor(UIColor.brown, for: .highlighted)
+        //给按钮添加target-action
+        button.addTarget(self, action: #selector(clicked), for: .touchUpInside)
+        
+        //将button加入视图控制器
+        self.view.addSubview(button)
     }
-
+    // 按钮点击事件，改变label显示的文字
+    @objc func clicked() {
+        if label.text == "hello world" {
+            label.text = "I'm clicked"
+        } else {
+            label.text = "hello world"
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
